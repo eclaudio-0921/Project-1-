f2481c7bcd3db176c7596056143f8826723de9c2
eclaudio-0921 <143681881+eclaudio-0921@users.noreply.github.com>
2023-10-01 21:55:38 -0500

    adding First Project HTML

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title> Eric A. CLaudio Resume  </title>
+  </head>
+  
+  <body>
+    <ul>
+      <li><a href="index.html"><strong>Resume</strong></a></li>
+      <li><a href="page2.html"><strong>God of War </strong></a></li>
+      <li><a href="page3.html"><strong>Super Mario Bros.</strong></a></li>
+	</ul>
+    <h1> Eric's Resume </h1>
+    <p> Eric A. Claudio  </p>
+    <p>eclaudio@hawk.iit.edu<p>
+    <h2> Education</h2>
+    <p>As of now attending the Chicago High School For Agricultural Sciences class of 2024<p>
+    <p>Enrolled in Fudamentals of Web Development as an enrollment course with IIT</p>
+    <img src="https://www.linkpicture.com/q/CHSAS.jpg" alt= "picture of Chicago Highschool for Agricultural Sciences logo" width="900" height="220">
+	<h3> Work Experience</h3>
+    <p>(NHS) National Honor Society- Service Learning</p>
+    <p>Depaul University-Summer Digital Arts program Location:Loop Campus</p>
+    <h4>Computer Science Skills</h4>
+    <p>HTML</p>
+    <p>CSS</p>
+  </body>
+  
+</html>
